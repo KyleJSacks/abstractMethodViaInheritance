b0e091312664a3dd11fb3f027f68a6e57fc1d08c
amy-lei <amy.lei2001@yahoo.com>
2019-03-01 13:55:51 -0500

    added to main method

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -2,5 +2,7 @@ public class UserOfSpeakers{
 	public static void main(String[] args){
 		Animal a = new Retriever();
 		System.out.println(a.speak());
+		Animal b = new Retriever();
+		System.out.println(b.speak());
 	}
 }
